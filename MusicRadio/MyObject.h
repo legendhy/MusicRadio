c439ec0b86705b21e9d8af2a012c2287bc87e9dd
huangyu <huangyu@huangyudemacbook-pro.local>
2016-08-24 13:28:17 +0800

    version 1: add    Alamofire,  KGFloatingDrawer  by cocoaPods                 add     AFSoundManager,  KVNProgress   (OC)

---
+++ MusicRadio/MyObject.h
@@ -0,0 +1,13 @@
+//
+//  MyObject.h
+//  MusicRadio
+//
+//  Created by huangyu on 16/8/24.
+//  Copyright © 2016年 huangyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyObject : NSObject
+
+@end
---
+++ MusicRadio/MyObject.m
@@ -0,0 +1,13 @@
+//
+//  MyObject.m
+//  MusicRadio
+//
+//  Created by huangyu on 16/8/24.
+//  Copyright © 2016年 huangyu. All rights reserved.
+//
+
+#import "MyObject.h"
+
+@implementation MyObject
+
+@end
