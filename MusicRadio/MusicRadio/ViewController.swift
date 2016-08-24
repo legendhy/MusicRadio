be51bbd42795d34fae2b2b4e40a2fb4b710ac2af
huangyu <huangyu@huangyudemacbook-pro.local>
2016-08-24 16:00:43 +0800

    KVNProgress test

--- MusicRadio/MusicRadio/ViewController.swift
+++ MusicRadio/MusicRadio/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        KVNProgress.setConfiguration(KVNProgressConfiguration.defaultConfiguration())
+        
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        KVNProgress.showWithStatus("This is a test!")
     }
 
     override func didReceiveMemoryWarning() {
